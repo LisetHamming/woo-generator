d756ae98425906d887fb7395e6ece7629dc4232c
Luis Anibal Villegas <luis@anibalism.com>
2024-02-27 19:25:07 +0100

    Naamwijziging van Woo naar Wob BES in gegenereerde brief

--- src/components/en/bes/popups/DataPopup.js
+++ src/components/en/bes/popups/DataPopup.js
@@ -3,7 +3,7 @@ const DataPopup = {
 		id: "1",
 		h1: "Are you a journalist?",
 		p:
-			"You don't have to be a journalist to submit a Woo request. However, it can be useful to mention it, as the European Court of Human Rights (ECHR) has determined that journalistic interest should be given special weight in the disclosure of documents in some cases."
+			"You don't have to be a journalist to submit a Wob BES  request. However, it can be useful to mention it, as the European Court of Human Rights (ECHR) has determined that journalistic interest should be given special weight in the disclosure of documents in some cases."
 	},
 	2: {
 		id: "2",
@@ -15,21 +15,21 @@ const DataPopup = {
 		id: "3",
 		h1: "On behalf of an (media) organization",
 		p:
-			"You can submit your Woo request on behalf of an (media) organization. Make sure you are authorized to do so and include the authorization with your request. The request may be rejected if you do not have the proper authorization. Also, organizations have a higher fee for appeals to the administrative court than individuals, which can save you a considerable amount. On the other hand, working for a well-known (media) organization can carry weight in the processing of your request. A compromise is to mention the (media) organization you work for without officially submitting the request on its behalf. Tick the option below in that case."
+			"You can submit your Wob BES request on behalf of an (media) organization. Make sure you are authorized to do so and include the authorization with your request. The request may be rejected if you do not have the proper authorization. Also, organizations have a higher fee for appeals to the administrative court than individuals, which can save you a considerable amount. On the other hand, working for a well-known (media) organization can carry weight in the processing of your request. A compromise is to mention the (media) organization you work for without officially submitting the request on its behalf. Tick the option below in that case."
 	},
 	4: {
 		id: "4",
 		h1: "Privacy and storage",
 		p:
-			"Everything you fill in is 'locally' stored through the browser you are using. That means no one else can see your answers, not even us. This is done through a so-called 'functional cookie'. Furthermore, a Google Analytics cookie is installed on the website to get an overview of the use of the Woo-generator."
+			"Everything you fill in is 'locally' stored through the browser you are using. That means no one else can see your answers, not even us. This is done through a so-called 'functional cookie'. Furthermore, a Google Analytics cookie is installed on the website to get an overview of the use of the Wob BES generator."
 	},
 	5: {
 		id: "5",
-		h1: "At which government agencies can I submit a Woo request with the Woo-generator?",
+		h1: "At which government agencies can I submit a Wob BES request with the Wob BES generator?",
 		p:
-			"There are numerous government agencies, also called 'government bodies,' in the Netherlands: municipalities, provinces, ministries, water boards, the police and inspection services, as well as so-called 'independent administrative bodies' such as the Social Insurance Bank (SVB) and the Central Bureau for Driving Skills (CBR). Under the Woo, as of May 1, 2022, several agencies have been added, including the First and Second Chamber, the Court of Audit, and the National Ombudsman. All these agencies fall under the Woo and can be found via the Woo-generator.",
+			"There are numerous government agencies, also called 'government bodies,' in the Netherlands: municipalities, provinces, ministries, water boards, the police and inspection services, as well as so-called 'independent administrative bodies' such as the Social Insurance Bank (SVB) and the Central Bureau for Driving Skills (CBR).",
 		p2:
-			"The database of government agencies falling under the Woo has been compiled by the members of the Woo working group of the VVOJ. As far as we know, it is the first such database in the Netherlands. All the mentioned agencies are included. However, this does not mean that the database is complete. It is possible that we have overlooked agencies, and government agencies can be abolished or change their names. Therefore, the database is continually evolving. Suggestions and updates are welcome: info@woo-generator.nl",
+			"The database of government agencies falling under the Wob BES  has been compiled by the members of the Wob BES  working group of the VVOJ. As far as we know, it is the first such database in the Netherlands. All the mentioned agencies are included. However, this does not mean that the database is complete. It is possible that we have overlooked agencies, and government agencies can be abolished or change their names. Therefore, the database is continually evolving. Suggestions and updates are welcome: info@Wob BES -generator.nl",
 		p3:
 			"If the government agency you are looking for is not listed, check the website of the relevant agency and enter the details manually."
 	},
@@ -39,7 +39,7 @@ const DataPopup = {
 		p:
 			"First, consider which government agency the documents on your subject can be found. If you are unsure, search online for your subject and see which government agencies come up. You can also submit the same request to different agencies. Sometimes, one government agency falls under the responsibility of another. For example, if you're not sure whether the documents are with the Immigration and Naturalization Service (IND) or the Ministry of Justice and Security, choose the 'highest' agency, in this case, the ministry. It's true that agencies that are less scrutinized may release documents more easily. So, think about where you have the best chance!",
 		p2:
-			"And rest assured: the agency where you submit your Woo request is obliged to forward your request to the correct agency. For added security, the generator reminds the government agency of this obligation in the 'referral' paragraph."
+			"And rest assured: the agency where you submit your Wob BES request is obliged to forward your request to the correct agency. For added security, the generator reminds the government agency of this obligation in the 'referral' paragraph."
 	},
 
 	7: {
@@ -52,13 +52,13 @@ const DataPopup = {
 		id: "8",
 		h1: "Why you need to be precise",
 		p:
-			"A too broad or unclear formulation of your subject may lead to the government requesting clarification. Under the Woo, the government agency must notify you of this within two weeks of receiving your request. Only after you have specified your request does the decision period begin. Unnecessary weeks may pass before the government processes your request."
+			"A too broad or unclear formulation of your subject may lead to the government requesting clarification. Under the Wob BES , the government agency must notify you of this within two weeks of receiving your request. Only after you have specified your request does the decision period begin. Unnecessary weeks may pass before the government processes your request."
 	},
 	9: {
 		id: "9",
 		h1: "Environment",
 		p:
-			"If your Woo request concerns environmental information, there are some special rules. Based on the Aarhus Convention, certain reasons for refusing environmental information do not or are less applicable. If you are unsure whether your request concerns environmental information, check Article 19.1a of the Environmental Management Act."
+			"If your Wob BES request concerns environmental information, there are some special rules. Based on the Aarhus Convention, certain reasons for refusing environmental information do not or are less applicable. If you are unsure whether your request concerns environmental information, check Article 19.1a of the Environmental Management Act."
 	},
 
 	11: {
@@ -67,7 +67,7 @@ const DataPopup = {
 		p:
 			"Choose this if you know exactly which document or documents you want. On the next page, you can describe these documents and also have the opportunity to request the disclosure of the underlying documents associated with a specific document. This way, you gain insight into the creation of the document(s), the individuals or organizations involved, changes in direction, etc.",
 		p2:
-			"To limit the complexity of the Woo-generator, it is not possible to check both options at the same time. If you genuinely want all documents on this subject, you will need to generate and send two separate letters. The government body may still treat both letters as one request. Keep in mind that the more documents you request, the longer it may take to receive them. Make sure to send both letters on the same day if you want the deadlines to be the same."
+			"To limit the complexity of the Wob BES generator, it is not possible to check both options at the same time. If you genuinely want all documents on this subject, you will need to generate and send two separate letters. The government body may still treat both letters as one request. Keep in mind that the more documents you request, the longer it may take to receive them. Make sure to send both letters on the same day if you want the deadlines to be the same."
 	},
 	12: {
 		id: "12",
@@ -81,7 +81,7 @@ const DataPopup = {
 		id: "13",
 		h1: "Documents",
 		p:
-			"'Documents' are fortunately broader than just written pieces. Excel sheets, data files, audio recordings, text messages, and WhatsApp messages are also included, for example. Of course, you can also ask the government for information that is not already on a data carrier, such as what the municipality thinks about a specific development or what the municipality plans to do about it. That is not a Woo request but a regular information request. The Woo-generator is not intended for this purpose."
+			"'Documents' are fortunately broader than just written pieces. Excel sheets, data files, audio recordings, text messages, and WhatsApp messages are also included, for example. Of course, you can also ask the government for information that is not already on a data carrier, such as what the municipality thinks about a specific development or what the municipality plans to do about it. That is not a Wob BES  request but a regular information request. The Wob BES generator is not intended for this purpose."
 	},
 	14: {
 		id: "14",
@@ -99,19 +99,19 @@ const DataPopup = {
 		id: "16",
 		h1: "The agency is not on the list",
 		p:
-			"If you know of a (government) agency that is not listed here, please send an email to info@woo-generator.com with the details of this agency so that we can add it."
+			"If you know of a (government) agency that is not listed here, please send an email to info@woo-generator.nl with the details of this agency so that we can add it."
 	},
 	17: {
 		id: "17",
 		h1: "Choosing a period",
 		p:
-			"By limiting the period, you prevent receiving irrelevant information, and Woo officials from being unnecessarily busy with your request."
+			"By limiting the period, you prevent receiving irrelevant information, and officials from being unnecessarily busy with your request."
 	},
 	18: {
 		id: "18",
 		h1: "Only public tasks",
 		p:
-			"You can only request information related to a public task. You can assume that everything a government agency deals with initially falls under the public task of that administrative body. Private matters of a civil servant do not fall under this. Information about claims for business trips does. The Woo is not suitable for information about yourself."
+			"You can only request information related to a public task. You can assume that everything a government agency deals with initially falls under the public task of that administrative body. Private matters of a civil servant do not fall under this. Information about claims for business trips does. The Wob BES  is not suitable for information about yourself."
 	},
 	19: {
 		id: "19",
